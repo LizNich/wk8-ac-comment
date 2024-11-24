9704c82893b4c49a9ef70b317fff2bdb01acf2ca
LizNich <nicholsonliz@hotmail.com>
2024-11-24 12:01:41 +0000

    add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import "./global.css";
+import { Gluten } from "next/font/google";
+
+export const gluten = Gluten({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex flex-col h-screen justify-between">
-          <header className="h-10  bg-red-500">
+      <body className={`${gluten.className} `}>
+        <div>
+          <header>
             <nav>
               <Link href="/">Welcome!</Link> |
               <Link href="/posts">All Posts</Link> |
@@ -14,7 +20,7 @@ export default function RootLayout({ children }) {
             </nav>
           </header>
           <main>{children}</main>
-          <footer className="h-10 bg-blue-500 text-center">
+          <footer>
             <p>&copy; The Animal Crossing Collective 2024</p>
           </footer>
         </div>
